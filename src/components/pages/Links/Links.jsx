630ca3f3c4465a4f7ca21a5a378a31717f94a78d
Adrian Litewski <litewskidev@gmail.com>
2023-09-30 06:49:10 +0200

    start implement links page

--- src/components/pages/Links/Links.jsx
+++ src/components/pages/Links/Links.jsx
@@ -1,17 +1,30 @@
 import { useSelector } from 'react-redux';
 import Navbar from '../../elements/Navbar/Navbar.jsx';
 import './Links.scss';
+import UserLink from '../../elements/UserLink/UserLink.jsx';
+import InputLink from '../../elements/InputLink/InputLink.jsx';
 
 const Links = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  
+
+  let linksArray = [];
+  if(userInfo.links !== undefined) {
+    linksArray = JSON.parse(userInfo.links);
+  }
 
   return(
     <section id='links'>
       <Navbar />
       <div className='links__wrapper'>
         <div className='links__mockup'>
-          <img src={process.env.PUBLIC_URL + '/assets/images/illustration-phone-mockup.svg'} alt='mobile phone mockup'/>
+          <div className='links__mockup__image'>
+            <img src={process.env.PUBLIC_URL + '/assets/images/illustration-phone-mockup.svg'} alt='mobile phone mockup'/>
+            <div className='links__mockup__links'>
+              {linksArray.map(link => (
+                <UserLink link={link.link} id={link.id} color={link.background} key={link.id} />
+              ))}
+            </div>
+          </div>
         </div>
         <div className='links__info'>
           <div className='links__info__container'>
@@ -22,13 +35,19 @@ const Links = () => {
             <div className='links__info__inner'>
               <button>+ Add new link</button>
               <div className='links__info__inner__list'>
-                <div className='links__info__inner__list__empty'>
-                  <img src={process.env.PUBLIC_URL + '/assets/images/illustration-empty.svg'} alt='mobile phone pic' />
-                  <div className='links__info__inner__list__empty__info'>
-                    <h2>Let's get you started</h2>
-                    <p>Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them. We're here to help you share your profiles with everyone!</p>
+                {(linksArray.length > 0) ? (
+                  linksArray.map(link => (
+                    <InputLink key={link.id} id={link.id}/>
+                  ))
+                ) : (
+                  <div className='links__info__inner__list__empty'>
+                    <img src={process.env.PUBLIC_URL + '/assets/images/illustration-empty.svg'} alt='mobile phone pic' />
+                    <div className='links__info__inner__list__empty__info'>
+                      <h2>Let's get you started</h2>
+                      <p>Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them. We're here to help you share your profiles with everyone!</p>
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
